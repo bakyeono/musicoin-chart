9ce941593c5e95c5a3556ebb30799ff353ad2dee
Bak Yeon O <bakyeono@gmail.com>
2020-03-28 21:22:26 +0900

    Update crawl command

--- musicoin/management/commands/crawl.py
+++ musicoin/management/commands/crawl.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from musicoin import crawler
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        crawler.crawl_and_update_song_infos()
+        self.stdout.write(self.style.SUCCESS(f'{timezone.localtime()} crawler start'))
+        try:
+            crawler.crawl_and_update_song_infos()
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f'{timezone.localtime()} crawler error: f{e}'))
+        self.stdout.write(self.style.SUCCESS(f'{timezone.localtime()} crawler end'))
